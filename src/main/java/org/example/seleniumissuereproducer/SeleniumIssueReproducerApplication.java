ee387fef2f89d3e892a5001af94898cebbccb55d
agarshu <shubham.agarwal@iss-stoxx.com>
2025-11-06 17:29:17 +0530

    setup

---
+++ src/main/java/org/example/seleniumissuereproducer/SeleniumIssueReproducerApplication.java
@@ -0,0 +1,13 @@
+package org.example.seleniumissuereproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumIssueReproducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumIssueReproducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/seleniumissuereproducer/SeleniumIssueReproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seleniumissuereproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumIssueReproducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
